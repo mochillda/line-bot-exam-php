5ffbd8ed556585a5cbdef85d0ab6ab3a6abe999a
mochillda <53145122+mochillda@users.noreply.github.com>
2020-01-08 16:20:01 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = 'qXqMgoayE5SSg4EbZZ/oBH47tDP8yUyx6E0jwaXffeS1FsARKNFvChIcdngxsn0
 
 $channelSecret = 'd4c772b3a8631c858948e11e6c1c1f78';
 
-$pushID = 'Uc143ca13b9733e2cfaee8a2c5d4482b8';
+$pushID = '';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);

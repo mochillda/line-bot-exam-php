58b4372635d4305603642673593e5800fd70f47b
mochillda <53145122+mochillda@users.noreply.github.com>
2020-01-08 15:58:23 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'qXqMgoayE5SSg4EbZZ/oBH47tDP8yUyx6E0jwaXffeS1FsARKNFvChIcdngxsn0g1hlvF/xWc5eza4ayr5jJQcoBDXmMwqqqzjCBS+xWT6Vvvdkjjtdtzm5gJAIQ9uS4J+JJ7NcKlXADiUe3DujDQgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'd4c772b3a8631c858948e11e6c1c1f78';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 

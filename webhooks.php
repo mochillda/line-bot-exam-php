9dc31b34f055c1c5342f29479e81831d8c1ed7a6
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-09 15:56:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,8 +23,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "wowwwww",
-				//'text' => $text
+				//'text' => "wowwwww",
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender

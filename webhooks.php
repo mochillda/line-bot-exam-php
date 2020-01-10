ea3087d6d610fc692d04e1b7546379accb6a713b
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-10 09:20:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -53,37 +53,37 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-// 		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'lo' ) {
-// 			$userId = $event['source']['userId'];
-// 			$text = '35.65910807942215,139.70372892916203';
-// 			$replyToken = $event['replyToken'];
-// 			print_r($event);
-// 			// Build message to reply back
-// 			$messages = [
-// 				'type' => 'text',
-// 				'text' => $text
-// 			];
-// 			$url = 'https://api.line.me/v2/bot/message/reply';
-// 			$data = [
-// 				'replyToken' => $replyToken,
-// 				'messages' => [$messages],
-// 			];
-// 			//print_r( $data );
-// 			$post = json_encode($data);
-// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'lo' ) {
+			$userId = $event['source']['userId'];
+			$text = '35.65910807942215,139.70372892916203';
+			$replyToken = $event['replyToken'];
+			print_r($event);
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			//print_r( $data );
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-// 			$ch = curl_init($url);
-// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// 			$result = curl_exec($ch);
-// 			curl_close($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-// 			echo $result . "\r\n";
+			echo $result . "\r\n";
 
-// 		}
+		}
 	}
 }
 echo "OK";

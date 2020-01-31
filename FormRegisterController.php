1064a48c2f9daa99d21f74c636f08f20800eb675
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-31 14:00:19 +0700

    Create FormRegisterController.php

---
+++ FormRegisterController.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class FormRegisterController {
+    public static $views = array();
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        echo 1;
+    }
+    
+}
+?>

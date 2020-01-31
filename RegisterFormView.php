0042fe03b11bf2d997e23dadd17485801ac5551c
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-01-31 13:49:30 +0700

    Update and rename index.html to RegisterFormView.php

--- RegisterFormView.php
+++ RegisterFormView.php
@@ -1,45 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>แบบฟอร์มลงทะเบียน</title>
+  <title><?php echo $this->lang->line("แบบฟอร์มลงทะเบียน");?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <!-- <?php $this->load->view('css'); ?> -->
+ 
 </head>
 <body>
-  <div class="container">
+   <div class="container">
       <div style="padding: 25px; text-align: center;">
-        <h2>แบบฟอร์มลงทะเบียน</h2>
+        <h2><?php echo $this->lang->line("แบบฟอร์มลงทะเบียน");?></h2>
       </div>
-      <form id="registerLine" class="form-horizontal" name="registerLine"  method="post"  action=""  >
+      <form id="registerLine" class="form-horizontal" name="registerLine"  method="post" > <!--action="<?php echo site_url($URL."/saveRegister") ;?>"-->
         <div class="box-body" style="padding: 25px;">
         <div class="form-group row">
-          <label for="codeStu" class="col-sm-2 col-form-label"><a>รหัสนักศึกษา : </a></label>
+          <label for="codeStu" class="col-sm-2 col-form-label"><a><?php echo $this->lang->line("รหัสนักศึกษา : ");?></a></label>
           <div class="col-sm-10">
             <input type="number" class="form-control" id="codeStu" placeholder="กรอกรหัสนักศึกษา" name="codeStu">
           </div>
         </div>
-        <div class="form-group row">
-          <label for="emailStu" class="col-sm-2 col-form-label">E-mail : </label>
+<!--         <div class="form-group row">
+          <label for="emailStu" class="col-sm-2 col-form-label"><?php echo $this->lang->line("E-mail : ");?></label>
           <div class="col-sm-10">
              <input type="text" class="form-control" id="emailStu" name="emailStu" placeholder="กรอก email">
           </div>
-        </div>
+        </div> -->
         <div style="text-align: center;">
-          <button type="button" class="btn btn-success">ลงทะเบียน</button>
+          <button type="button" class="btn btn-success"><?php echo $this->lang->line("ลงทะเบียน");?></button>
         </div>
        </div>
       </form>
   </div>
-
 </body>
 </html>
 
-<!-- <?php $this->load->view('script'); ?> -->
+<?php // $this->load->view('script'); ?>
 <script type="text/javascript">
 
   function checkEmail(str){

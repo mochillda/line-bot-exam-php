8c0eb959aafc05d1eae19b7ea20113eba520178f
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-02-04 10:26:06 +0700

    Update webhook4.php

--- webhook4.php
+++ webhook4.php
@@ -1,59 +1,89 @@
-<?php // callback.php
-
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
-$access_token = 'lMGsbFR4DBpd5t5u7AL31RqMxqM/tEe02YYOcf1TXOD1mprdXoPtkyRKbs+Q3APyFZVDLGZmxYXyZD7T1tvt0ztKmVRR73ngC3zduOM+r3qDohGFuNa5tV0aKp9M3GMFaPU3XEjPikIDPch8QLU0/QdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		
-			// Get text sent/
-			//print_r($event);
-			$userId = $event['source']['userId'];
-			$text = $event['message'][0]['text'];
-			//$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-// 				'text' => "wowwwww"
-				'text' => $userId
-// 				'text' => $userId
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				//'messages' => "wowwwww",
-				'messages' => [$messages],
-			];
-			print_r( $data );
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		
-	
-	}
+<?php
+function processMessage($update) {
+    if($update["queryResult"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Hello from webhook",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"response from host"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+    }else if($update["queryResult"]["action"] == "convert"){
+        if($update["queryResult"]["parameters"]["outputcurrency"] == "USD"){
+           $amount =  intval($update["queryResult"]["parameters"]["amountToConverte"]["amount"]);
+           $convertresult = $amount * 360;
+        }
+         sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"The conversion result is".$convertresult,
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"The conversion result is".$convertresult
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+    }else{
+        sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Error",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+        
+    }
 }
-echo "OK";
+ 
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+ 
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["queryResult"]["action"])) {
+    processMessage($update);
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+   fwrite($myfile, $update["queryResult"]["action"]);
+    fclose($myfile);
+}else{
+     sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Hello from webhook",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+}
+
+
+?>

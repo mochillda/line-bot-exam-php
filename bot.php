8daab2d9c5d095cac39bd1a7830cc729ad0f4645
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-05 14:53:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,47 +3,47 @@
 // include composer autoload
 require "vendor/autoload.php";
 // การตั้งเกี่ยวกับ bot
-require_once('bot_settings.php');
+// require_once('bot_settings.php');
 
-///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-//use LINE\LINEBot\Event;
-//use LINE\LINEBot\Event\BaseEvent;
-//use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
-use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
-use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
-use LINE\LINEBot\ImagemapActionBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
-use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
-use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use LINE\LINEBot\TemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+// ///////////// ส่วนของการเรียกใช้งาน class ผ่าน namespace
+// use LINE\LINEBot;
+// use LINE\LINEBot\HTTPClient;
+// use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+// //use LINE\LINEBot\Event;
+// //use LINE\LINEBot\Event\BaseEvent;
+// //use LINE\LINEBot\Event\MessageEvent;
+// use LINE\LINEBot\MessageBuilder;
+// use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+// use LINE\LINEBot\ImagemapActionBuilder;
+// use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+// use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
+// use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+// use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+// use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+// use LINE\LINEBot\TemplateActionBuilder;
+// use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+// use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+// use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+// use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+// use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 
 // เชื่อมต่อกับ LINE Messaging API
-$access_token = 'lMGsbFR4DBpd5t5u7AL31RqMxqM/tEe02YYOcf1TXOD1mprdXoPtkyRKbs+Q3APyFZVDLGZmxYXyZD7T1tvt0ztKmVRR73ngC3zduOM+r3qDohGFuNa5tV0aKp9M3GMFaPU3XEjPikIDPch8QLU0/QdB04t89/1O/w1cDnyilFU=';
-$httpClient = new CurlHTTPClient($access_token);
-$bot = new LINEBot($httpClient, array('channelSecret' => 'c26ca6b94dd522e9c9440326215124a5'));
+// $access_token = 'lMGsbFR4DBpd5t5u7AL31RqMxqM/tEe02YYOcf1TXOD1mprdXoPtkyRKbs+Q3APyFZVDLGZmxYXyZD7T1tvt0ztKmVRR73ngC3zduOM+r3qDohGFuNa5tV0aKp9M3GMFaPU3XEjPikIDPch8QLU0/QdB04t89/1O/w1cDnyilFU=';
+// $httpClient = new CurlHTTPClient($access_token);
+// $bot = new LINEBot($httpClient, array('channelSecret' => 'c26ca6b94dd522e9c9440326215124a5'));
 
 function processMessage($update) {
     if($update["queryResult"]["queryText"] == "ใช่"){

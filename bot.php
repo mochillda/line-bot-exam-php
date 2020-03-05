5c30bcd93d302eb01bef0b0f724442533928d5ea
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-05 09:21:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,27 +43,116 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 // เชื่อมต่อกับ LINE Messaging API
 $access_token = 'lMGsbFR4DBpd5t5u7AL31RqMxqM/tEe02YYOcf1TXOD1mprdXoPtkyRKbs+Q3APyFZVDLGZmxYXyZD7T1tvt0ztKmVRR73ngC3zduOM+r3qDohGFuNa5tV0aKp9M3GMFaPU3XEjPikIDPch8QLU0/QdB04t89/1O/w1cDnyilFU=';
 $httpClient = new CurlHTTPClient($access_token);
-$bot = new LINEBot($httpClient, array('channelSecret' => c26ca6b94dd522e9c9440326215124a5));
+$bot = new LINEBot($httpClient, array('channelSecret' => 'c26ca6b94dd522e9c9440326215124a5'));
 
-// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
-$content = file_get_contents('php://input');
- 
-// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
-$events = json_decode($content, true);
-if(!is_null($events)){
-    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
-    $replyToken = $events['events'][0]['replyToken'];
+function processMessage($update) {
+    if($update["queryResult"]["queryText"] == "ใช่"){
+        sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>$update['queryResult']['parameters'],
+//             "fulfillmentText"=>$update['queryResult']['parameters']['codeId'],
+           // "fulfillmentText"=>$update['originalDetectIntentRequest']['payload']['data']['source']['userId'],
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"response from host"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+    }else if($update["queryResult"]["queryText"] == "convert"){
+        if($update["queryResult"]["parameters"]["outputcurrency"] == "USD"){
+           $amount =  intval($update["queryResult"]["parameters"]["amountToConverte"]["amount"]);
+           $convertresult = $amount * 360;
+        }
+         sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"The conversion result is".$convertresult,
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"The conversion result is".$convertresult
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+    }else{
+        sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=> "Error",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+        
+    }
 }
-// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder(json_encode($events));
  
-//l ส่วนของคำสั่งตอบกลับข้อความ
-$response = $bot->replyMessage($replyToken,$textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
+function sendMessage($parameters) {
+    echo json_encode($parameters);
 }
  
-// Failed
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+// print_r(json_encode($update));
+if (isset($update["queryResult"]["queryText"])) {
+    processMessage($update);
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+   fwrite($myfile, $update["queryResult"]["queryText"]);
+    fclose($myfile);
+}else{
+     sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=> $update["queryResult"]["queryText"],
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+}
+
+// // คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
+// $content = file_get_contents('php://input');
+ 
+// // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+// $events = json_decode($content, true);
+// if(!is_null($events)){
+//     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+//     $replyToken = $events['events'][0]['replyToken'];
+// }
+// // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+// $textMessageBuilder = new TextMessageBuilder(json_encode($events));
+ 
+// //l ส่วนของคำสั่งตอบกลับข้อความ
+// $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+// if ($response->isSucceeded()) {
+//     echo 'Succeeded!';
+//     return;
+// }
+ 
+// // Failed
+// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>

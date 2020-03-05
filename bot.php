10d727168cb677c9f1f9d0baebb31864bcf40bb7
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-05 14:06:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,11 +58,9 @@ function processMessage($update) {
                     "type"=> 4,
                     "payload"=> array(
                        "line"=> array(
-                           "type"=> 'text',
-                           "text"=> 'ข้อความที่จะตอบกลับใน LINE'
-//                                 "type"=> 'sticker',
-//                                 "packageId"=> '1',
-//                                 "stickerId"=> '2579'
+                                "type"=> 'sticker',
+                                "packageId"=> '1',
+                                "stickerId"=> '2579'
                         )
                     )
                   )

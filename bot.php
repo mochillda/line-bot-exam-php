c95ff7783ab98906a70de4007f445efa4a913d95
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-06 15:40:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@ function processMessage($update) {
                            "payload"=> array(
                                           "line"=> array(
                                           "type"=> "text",
-                                          "text"=> $update['queryResult']['parameters']['codeId']//"ลงทะเบียนสำเร็จ"
+                                          "text"=> $update['queryResult']['parameters']['codeId']."!!!!!"//"ลงทะเบียนสำเร็จ"
                                           )
                                        )
                          )

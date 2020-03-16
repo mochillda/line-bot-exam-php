c0202ba8051d6af959eb8cf3447ea32958ceee16
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-16 14:13:03 +0700

    Update bot_card_student.php

--- bot_card_student.php
+++ bot_card_student.php
@@ -3,15 +3,62 @@
 function processMessage($update) {
        if (isset($update["queryResult"]["queryText"])) {
           if($update["queryResult"]["queryText"] == "บัตร นศ."){
+               sendMessage(array(
+                   "source" => $update["responseId"],
+                   "fulfillmentText"=> 'ข้อความที่จะตอบกลับแบบปกติ',
+                   "fulfillmentMessages"=> [
+                         array(
+                           "platform"=> 'line',
+                           "type"=> 4,
+                           "payload"=> array(
+                                          "line"=> array(
+                                          "type"=> "text",
+                                          "text"=> $update['queryResult']['parameters']['codeId']."ลงทะเบียนสำเร็จ"//"ลงทะเบียนสำเร็จ"
+                                          )
+                                       )
+                         )
+                       ],
+                   "payload" => array(
+                       "items"=>[
+                           array(
+                               "simpleResponse"=>
+                           array(
+                               "textToSpeech"=>"response from host"
+                                )
+                           )
+                       ],
+                       ),
+
+               ));
+          }else{
+               sendMessage(array(
+                   "source" => $update["responseId"],
+                   "fulfillmentText"=> 'ข้อความที่จะตอบกลับแบบปกติ',
+                   "fulfillmentMessages"=> [
+                         array(
+                           "platform"=> 'line',
+                           "type"=> 4,
+                           "payload"=> array(
+                                 "line"=> array(
+                                   "type"=> "template",
+                                   "altText"=> "This is a buttons template",
+                                   "template"=> array(
+                                       "type"=> "buttons",
+                                       "thumbnailImageUrl"=> "https://huntscholarships.com/wp-content/uploads/2012/08/panyapiwat.jpg",
+                                       "imageAspectRatio"=> "rectangle",
                                        "imageSize"=> "cover",
                                        "imageBackgroundColor"=> "#FFFFFF",
                                        "title"=> "คุณคือ ธิดารัตน์ ภู่ระหงษ์ ใช่หรือไม่?",
+       //                                 "title"=> $update["queryResult"]["parameters"]["param-name"] ,//test11.test11-custom.test11-custom-yes
+
                                        "text"=> "กรุณายืนยัน",
+       //                              "defaultAction"=> "",
                                        "actions"=> [
                                            array(
                                              "type"=> "postback",
                                              "label"=> "ใช่",
                                              "data"=> "action=buy&itemid=12",
+       //                                       "displayText"=>"ใช่"
                                               "text"=>'ใช่'
                                            ),
                                            array(

a134c5755479867a50520219d174cc0542c8a09a
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-16 15:29:23 +0700

    Update bot_card_student.php

--- bot_card_student.php
+++ bot_card_student.php
@@ -43,177 +43,55 @@ function processMessage($update) {
                            "payload"=> array(
                                   //--------------
                                  "line"=> array(
-                                   "type"=> "flex",
-                                     "altText"=> "Flex Message",
-                                     "contents"=> array(
-                                       "type"=> "bubble",
-                                       "hero"=> array(
-                                         "type"=> "image",
-                                         "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png",
-                                         "size"=> "full",
-                                         "aspectRatio"=> "20:13",
-                                         "aspectMode"=> "cover",
-                                         "action"=> array(
-                                           "type"=> "uri",
-                                           "label"=> "Action",
-                                           "uri"=> "https://linecorp.com/"
-                                         )
-                                       ),
-                                       "body"=> array(
-                                         "type"=> "box",
-                                         "layout"=> "vertical",
-                                         "spacing"=> "md",
-                                         "contents"=> [
-                                           array(
-                                             "type"=> "text",
-                                             "text"=> "BROWN'S ADVENTURE\nIN MOVIE",
-                                             "size"=> "xl",
-                                             "gravity"=> "center",
-                                             "weight"=> "bold",
-                                             "wrap"=> true
-                                           ),
-                                           array(
-                                             "type"=> "box",
-                                             "layout"=> "baseline",
-                                             "margin"=> "md",
-                                             "contents"=> [
-                                               array(
-                                                 "type"=> "icon",
-                                                 "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                                 "size"=> "sm"
-                                               ),
-                                               array(
-                                                 "type"=> "icon",
-                                                 "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                                 "size"=> "sm"
-                                               ),
-                                               array(
-                                                 "type"=> "icon",
-                                                 "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                                 "size"=> "sm"
-                                               ),
-                                               array(
-                                                 "type"=> "icon",
-                                                 "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                                                 "size"=> "sm"
-                                               ),
-                                               array(
-                                                 "type"=> "icon",
-                                                 "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-                                                 "size"=> "sm"
-                                               ),
-                                               array(
-                                                 "type"=> "text",
-                                                 "text"=> "4.0",
-                                                 "flex"=> 0,
-                                                 "margin"=> "md",
-                                                 "size"=> "sm",
-                                                 "color"=> "#999999"
-                                               )
-                                             ]
-                                           ),
-                                           array(
-                                             "type"=> "box",
-                                             "layout"=> "vertical",
-                                             "spacing"=> "sm",
-                                             "margin"=> "lg",
-                                             "contents"=> [
-                                               array(
-                                                 "type"=> "box",
-                                                 "layout"=> "baseline",
-                                                 "spacing"=> "sm",
-                                                 "contents"=> [
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "Date",
-                                                     "flex"=> 1,
-                                                     "size"=> "sm",
-                                                     "color"=> "#AAAAAA"
-                                                   ),
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "Monday 25, 9:00PM",
-                                                     "flex"=> 4,
-                                                     "size"=> "sm",
-                                                     "color"=> "#666666",
-                                                     "wrap"=> true
-                                                   )
-                                                 ]
-                                               ),
-                                               array(
-                                                 "type"=> "box",
-                                                 "layout"=> "baseline",
-                                                 "spacing"=> "sm",
-                                                 "contents"=> [
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "Place",
-                                                     "flex"=> 1,
-                                                     "size"=> "sm",
-                                                     "color"=> "#AAAAAA"
-                                                   },
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "7 Floor, No.3",
-                                                     "flex"=> 4,
-                                                     "size"=> "sm",
-                                                     "color"=> "#666666",
-                                                     "wrap"=> true
-                                                   )
-                                                 ]
-                                               ),
-                                               array(
-                                                 "type"=> "box",
-                                                 "layout"=> "baseline",
-                                                 "spacing"=> "sm",
-                                                 "contents"=> [
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "Seats",
-                                                     "flex"=> 1,
-                                                     "size"=> "sm",
-                                                     "color"=> "#AAAAAA"
-                                                   ),
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "C Row, 18 Seat",
-                                                     "flex"=> 4,
-                                                     "size"=> "sm",
-                                                     "color"=> "#666666",
-                                                     "wrap"=> true
-                                                   )
-                                                 ]
-                                               ),
-                                                array(
-                                                 "type"=> "box",
-                                                 "layout"=> "vertical",
-                                                 "margin"=> "xxl",
-                                                 "contents"=> [
-                                                   array(
-                                                     "type"=> "spacer"
-                                                   ),
-                                                   array(
-                                                     "type"=> "image",
-                                                     "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png",
-                                                     "size"=> "xl",
-                                                     "aspectMode"=> "cover"
-                                                   ),
-                                                   array(
-                                                     "type"=> "text",
-                                                     "text"=> "You can enter the theater by using this code instead of a ticket",
-                                                     "margin"=> "xxl",
-                                                     "size"=> "xs",
-                                                     "color"=> "#AAAAAA",
-                                                     "wrap"=> true
-                                                   )
-                                                 ]
-                                               )
-                                             ]
-                                           )
+                                       "type": "flex",
+                                     "altText": "Flex Message",
+                                     "contents": {
+                                       "type": "bubble",
+                                       "direction": "ltr",
+                                       "header": {
+                                         "type": "box",
+                                         "layout": "vertical",
+                                         "contents": [
+                                           {
+                                             "type": "text",
+                                             "text": "Header",
+                                             "align": "center"
+                                           }
                                          ]
-                                       )
-                                     )
-                                        
+                                       },
+                                       "hero": {
+                                         "type": "image",
+                                         "url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+                                         "size": "full",
+                                         "aspectRatio": "1.51:1",
+                                         "aspectMode": "fit"
+                                       },
+                                       "body": {
+                                         "type": "box",
+                                         "layout": "vertical",
+                                         "contents": [
+                                           {
+                                             "type": "text",
+                                             "text": "Body",
+                                             "align": "center"
+                                           }
+                                         ]
+                                       },
+                                       "footer": {
+                                         "type": "box",
+                                         "layout": "horizontal",
+                                         "contents": [
+                                           {
+                                             "type": "button",
+                                             "action": {
+                                               "type": "uri",
+                                               "label": "Button",
+                                               "uri": "https://linecorp.com"
+                                             }
+                                           }
+                                         ]
+                                       }
+                                     }
                                           
 
                                   )

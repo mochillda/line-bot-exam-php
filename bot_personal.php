0f86ae97b5f98d6d70dfc77850966fad3202bb13
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-03-26 00:54:37 +0700

    Update bot_personal.php

--- bot_personal.php
+++ bot_personal.php
@@ -16,164 +16,21 @@ function processMessage($update) {
                                           "line"=> array(
                                           "type"=> "text",
                                           "text"=> $update['queryResult']['parameters']['codeId']."ลงทะเบียนสำเร็จ"//"ลงทะเบียนสำเร็จ"
-                                          )
                                        )
+                            )
                          )
                        ],
-                   "payload" => array(
-                       "items"=>[
-                           array(
-                               "simpleResponse"=>
-                           array(
-                               "textToSpeech"=>"response from host"
-                                )
-                           )
-                       ],
+                       "payload" => array(
+                              "items"=>[
+                                  array(
+                                      "simpleResponse"=>
+                                         array(
+                                             "textToSpeech"=>"response from host"
+                                              )
+                                  )
+                              ],
                        ),
-
                ));
-//           }else if($update["queryResult"]["queryText"] == "คำถาม"){
-//                sendMessage(array(
-//                    "source" => $update["responseId"],
-//                    "fulfillmentText"=> 'ข้อความที่จะตอบกลับแบบปกติ',
-//                    "fulfillmentMessages"=> [
-//                          array(
-//                            "platform"=> 'line',
-//                            "type"=> 4,
-//                            "payload"=> array(
-//                                  "line"=> array(
-//                                      "type"=> "flex",
-//                 "altText"=> "Flex Message",
-//                 "contents"=> array(
-//                   "type": "carousel",
-//                   "contents"=> [
-//                     array(
-//                       "type"=> "bubble",
-//                       "hero"=> array(
-//                         "type"=> "image",
-//                         "url"=> "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-//                         "size"=> "full",
-//                         "aspectRatio"=> "20:13",
-//                         "aspectMode"=> "cover"
-//                       ),
-//                       "body"=> array(
-//                         "type"=> "box",
-//                         "layout"=> "vertical",
-//                         "spacing"=> "sm",
-//                         "contents"=> [
-//                           array(
-//                             "type"=> "text",
-//                             "text"=> "Arm Chair, White",
-//                             "size"=> "xl",
-//                             "weight"=>"bold",
-//                             "wrap"=> true
-//                           ),
-//                           array(
-//                             "type"=> "box",
-//                             "layout"=> "baseline",
-//                             "contents"=> [
-//                               array(
-//                                 "type"=> "text",
-//                                 "text"=> "$49",
-//                                 "flex"=> 0,
-//                                 "size"=> "xl",
-//                                 "weight"=> "bold",
-//                                 "wrap"=> true
-//                               ),
-//                               array(
-//                                 "type"=> "text",
-//                                 "text"=> ".99",
-//                                 "flex"=> 0,
-//                                 "size"=> "sm",
-//                                 "weight"=> "bold",
-//                                 "wrap"=> true
-//                               )
-//                             ]
-//                           )
-//                         ]
-//                       ),
-//                       "footer"=> array(
-//                         "type"=>"box",
-//                         "layout"=> "vertical",
-//                         "spacing"=> "sm",
-//                         "contents"=> [
-//                           array(
-//                             "type"=> "button",
-//                             "action"=> array(
-//                               "type"=> "uri",
-//                               "label"=> "Add to Cart",
-//                               "uri"=> "https://linecorp.com"
-//                             ),
-//                             "style"=> "primary"
-//                           ),
-//                           array(
-//                             "type"=> "button",
-//                             "action"=> array(
-//                               "type"=> "uri",
-//                               "label"=> "Add to whishlist",
-//                               "uri"=> "https://linecorp.com"
-//                             )
-//                           )
-//                         ]
-//                       )
-//                     ),
-//             array(
-//               "type"=> "text",
-//               "text"=> "Temporarily out of stock",
-//               "flex"=> 0,
-//               "margin"=> "md",
-//               "size"=> "xxs",
-//               "color"=> "#FF5551",
-//               "wrap"=> true
-//             )
-//           ]
-//         ),
-//         "footer"=> array(
-//           "type"=> "box",
-//           "layout"=> "vertical",
-//           "spacing"=> "sm",
-//           "contents"=> [
-//             (
-//               "type"=> "button",
-//               "action"=> array(
-//                 "type"=> "uri",
-//                 "label"=> "Add to Cart",
-//                 "uri"=> "https://linecorp.com"
-//               ),
-//               "flex"=> 2,
-//               "color"=> "#AAAAAA",
-//               "style"=> "primary"
-//             ),
-//             array(
-//               "type"=> "button",
-//               "action"=> array(
-//                 "type"=> "uri",
-//                 "label"=> "Add to wish list",
-//                 "uri"=> "https://linecorp.com"
-//               )
-//             )
-//           ]
-//         )
-//       )
-//     ]
-//   )
-//                                  )
-
-//                            )
-//                          )
-//                        ],
-//                    "payload" => array(
-//                        "items"=>[
-//                            array(
-//                                "simpleResponse"=>
-//                            array(
-//                                "textToSpeech"=>"response from host"
-//                                 )
-//                            )
-//                        ],
-//                        ),
-
-//                ));
           }else{
                sendMessage(array(
                    "source" => $update["responseId"],
@@ -183,6 +40,7 @@ function processMessage($update) {
                            "platform"=> 'line',
                            "type"=> 4,
                            "payload"=> array(
+                                  
                                  "line"=> array(
                                      "type"=> "template",
                                      "altText"=> "this is a buttons template",
@@ -214,21 +72,20 @@ function processMessage($update) {
                                               "title"=> "ข้อมูลนักศึกษา",
                                               "text"=> "กรุณาเลือก"
                                      )
-
                                  )
 
                            )
                          )
                        ],
-                   "payload" => array(
-                       "items"=>[
-                           array(
-                               "simpleResponse"=>
-                           array(
-                               "textToSpeech"=>"response from host"
-                                )
-                           )
-                       ],
+                       "payload" => array(
+                              "items"=>[
+                                  array(
+                                      "simpleResponse"=>
+                                         array(
+                                             "textToSpeech"=>"response from host"
+                                              )
+                                  )
+                              ],
                        ),
 
                ));

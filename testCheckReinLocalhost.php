42405ed33828741640d46b1a1dbfa6616bd9c7fa
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-05-08 17:21:37 +0700

    Update testCheckReinLocalhost.php

--- testCheckReinLocalhost.php
+++ testCheckReinLocalhost.php
@@ -3,10 +3,10 @@
 function processMessage($update) {
        if (isset($update["queryResult"]["queryText"])) {
           if($update["queryResult"]["queryText"] == "ลงทะเบียน / ข้อมูลการลงทะเบียน"){
-               $this->sendMessage(array(
+              sendMessage(array(
                    "source" => $update["responseId"],
                    "fulfillmentText"=> 'ข้อความที่จะตอบกลับแบบปกติ',
-                   "fulfillmentMessages"=> (
+                   "fulfillmentMessages"=> [
                          array(
                            "platform"=> 'line',
                            "type"=> 'buttons',
@@ -17,32 +17,32 @@ function processMessage($update) {
                                        )
                             )
                          )
-                       ),
+                       ],
                        "payload" => array(
-                              "items"=>(
+                              "items"=>[
                                   array(
                                       "simpleResponse"=>
                                          array(
                                              "textToSpeech"=>"response from host"
                                               )
                                   )
-                              ),
+                              ],
                        ),
                ));
           }
         }else{
-            $this->sendMessage(array(
+            sendMessage(array(
                 "source" => $update["responseId"],
                 "fulfillmentText"=> "Error",
                 "payload" => array(
-                    "items"=>(
+                    "items"=>[
                         array(
                             "simpleResponse"=>
                         array(
                             "textToSpeech"=>"Bad request"
                              )
                         )
-                    ),
+                    ],
                     ),
                
             ));
@@ -55,29 +55,29 @@ function processMessage($update) {
         $update = json_decode($update_response, true);
 
         if (isset($update["queryResult"]["queryText"])) {
-            $this->processMessage($update);
+            processMessage($update);
             $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
             fwrite($myfile, $update["queryResult"]["queryText"]);
             fclose($myfile);
         }else{
-            $this->sendMessage(array(
+            sendMessage(array(
                     "source" => $update["responseId"],
                     "fulfillmentText"=> $update["queryResult"]["queryText"],
                     "payload" => array(
-                        "items"=>(
+                        "items"=>[
                             array(
                                 "simpleResponse"=>
                             array(
                                 "textToSpeech"=>"Bad request"
                                  )
                             )
-                        ),
+                        ],
                         ),
                 ));
         }
     }
 
-    public function sendMessage($parameters = array()) {
+    public function sendMessage($parameters) {
         echo json_encode($parameters);
     }   
 

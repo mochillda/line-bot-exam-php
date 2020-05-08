2e3bebf383e912d3c5b3808092f652df311c8103
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-05-08 18:15:30 +0700

    Update testCheckReinLocalhost.php

--- testCheckReinLocalhost.php
+++ testCheckReinLocalhost.php
@@ -51,7 +51,7 @@
     }
  
 //     function webhook(){ 
-        $update_response = file_get_contents('php://input');
+        $update_response = file_get_contents("php://input");
         $update = json_decode($update_response, true);
 
         if (isset($update["queryResult"]["queryText"])) {

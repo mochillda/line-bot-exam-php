232853fd85d1e76ebb14d3eb7b7c6620effd881c
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-05-08 18:17:44 +0700

    Update testCheckReinLocalhost.php

--- testCheckReinLocalhost.php
+++ testCheckReinLocalhost.php
@@ -17,7 +17,7 @@
                                        )
                             )
                          )
-                       ),
+                       ],
                        "payload" => array(
                               "items"=>[
                                   array(

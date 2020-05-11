3dc8aca415d30d7c0e335468514acea8ce1de4b3
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-05-11 16:13:07 +0700

    Update testWebhookMasterFullfelment.php

--- testWebhookMasterFullfelment.php
+++ testWebhookMasterFullfelment.php
@@ -5,86 +5,65 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'lMGsbFR4DBpd5t5u7AL31RqMxqM/tEe02YYOcf1TXOD1mprdXoPtkyRKbs+Q3APyFZVDLGZmxYXyZD7T1tvt0ztKmVRR73ngC3zduOM+r3qDohGFuNa5tV0aKp9M3GMFaPU3XEjPikIDPch8QLU0/QdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'id') {
-			// Get text sent/
-			//print_r($event);
-			$userId = $event['source']['userId'];
-			$text = $event['message'][0]['text'];
-			//$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+ $content = file_get_contents('php://input');
+        // Parse JSON
+        $event = json_decode($content, true);
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-// 				'text' => "wowwwww"
-				'text' => $userId
-// 				'text' => $userId
-			];
+                if ($event["queryResult"]["queryText"] == 'ลงทะเบียน / ข้อมูลการลงทะเบียน') {
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				//'messages' => "wowwwww",
-				'messages' => [$messages],
-			];
-			print_r( $data );
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                    $userId = $event['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+                    $text   = $event['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+                    //$text = $event['source']['userId'];
+                    // Get replyToken
+                    $replyToken = $event["responseId"];
+                    // Build message to reply back
+                    $datas = array();
+                    $datas['type'] = 'text';
+                    $datas['text'] = $userId;
+                    // Make a POST Request to Messaging API to reply to sender
+                    $url = 'https://api.line.me/v2/bot/message/reply';
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+                    $messages = array();
+                    $messages['replyToken'] = $replyToken;
+                    $messages['messages'][] = $datas;
+                    $messages['notificationDisabled'] = false;
+                    
+                    $encodeJson = json_encode($messages);
 
-			echo $result . "\r\n";
-		}
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'lo' ) {
-			$userId = $event['source']['userId'];
-			//$text = '35.65910807942215,139.70372892916203';
-			$replyToken = $event['replyToken'];
-			$text = $replyToken;
-			print_r($event);
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			//print_r( $data );
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                    $headers = array('content-type: application/json; charset=UTF-8'
+                                    ,"cache-control: no-cache"
+                                    , 'Authorization: Bearer ' . $access_token);
+                    
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_PROXY, PROXY);
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, $encodeJson);
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                    
+                    $result = curl_exec($ch);
+                    $err = curl_error($ch);
+                    curl_close($ch);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+                    $datasReturn = array();
+			    if ($err) {
+				$datasReturn['result'] = 'E';
+				$datasReturn['message'] = $err;
+			    } else {
+				if($result == "{}"){
+				    $datasReturn['result'] = 'S';
+				    $datasReturn['message'] = 'Success';
+				}else{
+				    $datasReturn['result'] = 'E';
+				    $datasReturn['message'] = $result;
+				}
+			    }
+                    
+                }else{
 
-			echo $result . "\r\n";
-
-		}
-	}
-}
-echo "OK";
+                    echo "FALSE NO ID";
+                }

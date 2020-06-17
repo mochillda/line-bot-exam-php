cfc10a69d3292f9798d71d0cb055d5b6394b9ee5
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-06-17 14:04:36 +0700

    Update testWebhookMasterFullfelment.php

--- testWebhookMasterFullfelment.php
+++ testWebhookMasterFullfelment.php
@@ -17,7 +17,7 @@ $event = json_decode($content, true);
 
 			$messages = [
 				'type' => 'text',
-				'text' => $userId
+				'text' => $event['originalDetectIntentRequest']['payload']['data']['source']//$userId
 			];
 
 			// Make a POST Request to Messaging API to reply to sender

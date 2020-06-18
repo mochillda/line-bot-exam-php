a8cb5b9a75640dc23da8e2965f19fbe1b7b9f43f
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-06-18 23:35:50 +0700

    Update testScanner.php

--- testScanner.php
+++ testScanner.php
@@ -17,12 +17,27 @@
   <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
   <script>
     function scanCode() {
-//       alert('dddddd');
-      liff.scanCode().then(result => {
-        const stringifiedResult = JSON.stringify(result);
-        alert(stringifiedResult);
-        document.getElementById("scanCode").textContent = stringifiedResult;
+    	  liff.sendMessages([
+        {
+          type: 'text',
+          text: 'From:' + profile.displayName
+        }
+        ]).then(function () {
+        liff.closeWindow();
+        }).catch(function (error) {
+        window.alert('Error sending message: ' + error.message);
+        });
+      }).catch(function (error) {
+        window.alert("Error getting profile: " + error.message);
       });
     }
-    liff.init({ liffId: "https://liff.line.me/1653870917-7bBqe2GM" }, () => {}, err => console.error(err.code, error.message));
+//     function scanCode() {
+// //       alert('dddddd');
+//       liff.scanCode().then(result => {
+//         const stringifiedResult = JSON.stringify(result);
+//         alert(stringifiedResult);
+//         document.getElementById("scanCode").textContent = stringifiedResult;
+//       });
+//     }
+//     liff.init({ liffId: "https://liff.line.me/1653870917-7bBqe2GM" }, () => {}, err => console.error(err.code, error.message));
   </script>

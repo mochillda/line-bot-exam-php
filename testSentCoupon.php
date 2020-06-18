cc2b4d1255fb779306d23fb948dd33b7e3ae517b
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-06-19 00:38:43 +0700

    Update testSentCoupon.php

--- testSentCoupon.php
+++ testSentCoupon.php
@@ -91,7 +91,7 @@
 			<div class="user_card">
 				<div class="d-flex justify-content-center">
 					<div class="brand_logo_container">
-						<img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo">
+						<img src="https://c7.uihere.com/files/923/473/200/recycling-symbol-logo-clip-art-recycle.jpg" class="brand_logo" alt="Logo">
 					</div>
 				</div>
 				<div class="d-flex justify-content-center form_container">
@@ -108,26 +108,26 @@
 							</div>
 							<input type="password" name="" class="form-control input_pass" value="" placeholder="password">
 						</div>
-						<div class="form-group">
+<!-- 						<div class="form-group">
 							<div class="custom-control custom-checkbox">
 								<input type="checkbox" class="custom-control-input" id="customControlInline">
 								<label class="custom-control-label" for="customControlInline">Remember me</label>
 							</div>
-						</div>
+						</div> -->
 							<div class="d-flex justify-content-center mt-3 login_container">
 				 	<button type="button" name="button" class="btn login_btn">Login</button>
 				   </div>
 					</form>
 				</div>
 		
-				<div class="mt-4">
+<!-- 				<div class="mt-4">
 					<div class="d-flex justify-content-center links">
 						Don't have an account? <a href="#" class="ml-2">Sign Up</a>
 					</div>
 					<div class="d-flex justify-content-center links">
 						<a href="#">Forgot your password?</a>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>

a4d6987e56e67326ba56ff742a3ea12372cbd324
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-06-18 23:42:55 +0700

    Update testScanner.php

--- testScanner.php
+++ testScanner.php
@@ -16,29 +16,9 @@
 
   <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
   <script>
-    alert('gggg');
-//     function scanCode() {
-//     	  liff.sendMessages([
-//         {
-//           type: 'text',
-//           text: 'From:' + profile.displayName
-//         }
-//         ]).then(function () {
-//         liff.closeWindow();
-//         }).catch(function (error) {
-//         window.alert('Error sending message: ' + error.message);
-//         });
-//       }).catch(function (error) {
-//         window.alert("Error getting profile: " + error.message);
-//       });
+    
+    function scanCode() {
+      alert('gggg');
     }
-//     function scanCode() {
-// //       alert('dddddd');
-//       liff.scanCode().then(result => {
-//         const stringifiedResult = JSON.stringify(result);
-//         alert(stringifiedResult);
-//         document.getElementById("scanCode").textContent = stringifiedResult;
-//       });
-//     }
-//     liff.init({ liffId: "https://liff.line.me/1653870917-7bBqe2GM" }, () => {}, err => console.error(err.code, error.message));
+
   </script>

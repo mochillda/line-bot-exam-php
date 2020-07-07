9baa112ba51907652fc1e7194b4414cb6724a942
MoZilla <53145122+mochillda@users.noreply.github.com>
2020-07-07 13:53:18 +0700

    Update test_card_student.php

--- test_card_student.php
+++ test_card_student.php
@@ -15,7 +15,7 @@ function processMessage($update) {
                            "payload"=> array(
                                  "line"=> array(
                                  "type"=> "text",
-                                 "text"=> "test tatataaaaaaa" 
+                                 "text"=> "testttt" 
                             )
                            )
                          )
